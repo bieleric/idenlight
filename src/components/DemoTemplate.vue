49606ab27ad8d9bfbc16ae847136f8c4020f4018
bieleric <biele.eric@gmail.com>
2023-10-31 11:48:26 +0100

    [ADD] demo template

---
+++ src/components/DemoTemplate.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="row col-10 mx-auto mt-5 mb-3 h-75">
+        <div class="col-md-8 col-12 mx-auto bg-color-secondary text-light h-100">
+            DemoTemplate
+        </div>
+        <div class="col-md-4 col-12 mx-auto bg-color-third text-light h-100">
+            Action Box
+        </div>
+    </div>
+    <div class="row gx-5 col-10 mx-auto justify-content-end">
+        <div class="col-1">
+            <div class="px-4 btn btn-primary">Zurück</div>
+        </div>
+        <div class="col-1">
+            <div class="px-4 btn btn-primary">Weiter</div>
+        </div>
+    </div>
+</template>
--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,10 +1,13 @@
+<script setup>
+import DemoTemplate from '../components/DemoTemplate.vue';
+
+</script>
+
 <template>
   <RouterLink to="/home">
     <div id="header" class="col-md-2 col-4 mx-auto mt-3"><img src="/idenlight_dark.png" alt="IDEnlight" class="w-100"></div>
   </RouterLink>
-  <div>
-    
-  </div>
+  <DemoTemplate></DemoTemplate>
 </template>
 
 <style>
